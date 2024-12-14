672f4ee6f74a920d43e530966e1817d2a21fb7c1
soundarya8754 <soundaryaece37@gmail.com>
2024-12-14 21:52:19 +0530

    old one

--- src/main/java/sample/demo/SampleProjectApplication.java
+++ src/main/java/sample/demo/SampleProjectApplication.java
@@ -4,15 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RestController
 public class SampleProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleProjectApplication.class, args);
 	}
         
-	@Getmapping("/")
-	public welcomepage(){
-		return "welcome to my page";
-	}
 }
