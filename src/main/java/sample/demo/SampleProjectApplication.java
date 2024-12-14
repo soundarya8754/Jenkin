e5e82e64fdecd542d167341f8e5d02e1ebe58259
soundarya8754 <soundaryaece37@gmail.com>
2024-12-14 21:36:51 +0530

    code change

--- src/main/java/sample/demo/SampleProjectApplication.java
+++ src/main/java/sample/demo/SampleProjectApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class SampleProjectApplication {
 
+        Run|Debug
 	public static void main(String[] args) {
 		SpringApplication.run(SampleProjectApplication.class, args);
 	}
-
+        
+        @Getmapping("/")
+        public welcomepage(){
+		return "welcome to my page";
+	}
 }
